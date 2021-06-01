431a25d659699dc337f73205cac7c63b7eb865cb
Timothy Masek <timothykmasek@gmail.com>
2021-06-01 13:05:51 +0100

    index / show & create rackets

--- app/controllers/rackets_controller.rb
+++ app/controllers/rackets_controller.rb
@@ -1,14 +1,25 @@
 class RacketsController < ApplicationController
+  before_action :set_racket, only: [:edit, :update, :destroy]
+
   def index
+    @rackets = Racket.all
   end
 
   def show
+    @racket
   end
 
   def new
+    @racket = Racket.new
   end
 
   def create
+    @racket = Racket.new(racket_params)
+    if @racket.save
+      redirect_to racket_path(@racket)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,6 +33,12 @@ class RacketsController < ApplicationController
 
   private
 
-  def racket_params
+  def set_racket
+    @racket = Racket.find(params[:id])
   end
+
+  def racket_params
+    params.require(:racket).permit(:name, :location, :description, :brand, :image_url)
+  end
+
 end
